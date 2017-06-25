1bef9662467b3e338fb0b908f37ac0e45cef2b4f
Vivek Gupta <mail@vivekgupta.com>
2017-06-25 16:23:44 +0530

    removed comment

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -20,11 +20,6 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = '/home';
 
     /**
